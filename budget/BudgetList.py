e7558c86227c105d9795f105126954cbdd55b994
teeokwo <115704212+teeokwo@users.noreply.github.com>
2022-10-18 11:15:39 +0100

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,4 +1,5 @@
 from . import Expense
+import matplotlib.pyplot as plt
 class BudgetList():
     def __init__(self, budget):
         self.budget = budget
@@ -21,7 +22,16 @@ class BudgetList():
     def __len__(self):
        return (len(self.expenses) + len(self.overages))
         
-    
+    def __iter__(self):
+     self.iter_e = iter(self.expenses)
+     self.iter_o = iter(self.overages)
+     return self 
+
+    def __next__(self):
+        try:
+            return next(self.iter_e)
+        except StopIteration as stop:
+            return next(self.iter_o)
 
 def main():
     myBudgetList = BudgetList(1200)
@@ -29,14 +39,21 @@ def main():
     expenses.read_expenses('data/spending_data.csv')
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
-    print('The count of all expenses: '+ str(len(myBudgetList)))
-
+        print('The count of all expenses: '+ str(len(myBudgetList)))
+    for entry in myBudgetList:
+        print(entry)
+    # create figure and axis
+    (fig, ax) = plt.subplots()
+    labels = ['Expenses', 'Overages', 'Budget']
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages,myBudgetList.budget]
+    #create barchart
+    ax.bar(labels,values,color=['green', 'red', 'blue'])
+    ax.set_title('Your total expenses vs your total budget')
+    plt.show()
 if (__name__ == '__main__'):
-    main()
+     main()
 
-def __iter__(self):
-     iter(self.expense)
-     self.iter_o = iter(self.overages)
-     return self 
+
+
       
         
