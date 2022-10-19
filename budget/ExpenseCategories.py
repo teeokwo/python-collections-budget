b40183a53f6ad3de73b5bb4ac0c9bdad114bc7d8
teeokwo <115704212+teeokwo@users.noreply.github.com>
2022-10-19 09:54:53 +0100

    Update ExpenseCategories.py

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -9,7 +9,7 @@ def main():
     if(divided_for_loop != divided_set_comp):
         print('Sets are NOT equal by == test')
     for a,b in zip(divided_for_loop, divided_set_comp):
-         if not (a.issubset(b)):
+         if (not (a.issubset(b) and b.issubset(a))) :
              print('Sets are NOT equal by subset test')
 if __name__ == "__main__":
     main()
