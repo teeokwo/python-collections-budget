9aceece95e2c75ad88471d214591a5bbe89bd913
teeokwo <115704212+teeokwo@users.noreply.github.com>
2022-10-19 09:50:12 +0100

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -29,7 +29,7 @@ class BudgetList():
 
     def __next__(self):
         try:
-            return self.iter_e.__next_()
+            return self.iter_e.__next__()
         except StopIteration as stop:
             return self.iter_o.__next__()
 
